d5b4764699a80453fb834f348c41279db8838f9e
yopiidev <yopiidev@gmail.com>
2023-07-17 21:59:50 +0900

    DataAccess.Azure.Cosmos: update namespace for extensions

--- sources/AppSightNet.DataAccess.Azure.Cosmos/Stores/Extensions/CosmosStoreServiceCollectionExtensions.cs
+++ sources/AppSightNet.DataAccess.Azure.Cosmos/Stores/Extensions/CosmosStoreServiceCollectionExtensions.cs
@@ -2,7 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
-namespace AppSightNet.DataAccess.Azure.Cosmos.Stores;
+namespace AppSightNet.DataAccess.Azure.Cosmos.Stores.Extensions;
 
 public static class CosmosStoreServiceCollectionExtensions
 {
